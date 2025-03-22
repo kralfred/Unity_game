ad7fa3bed1bcc57f49b0ca791d265eb7aa29eb2c
alfred <alfred.withers@seznam.cz>
2025-03-22 19:16:54 +0100

    finished clock animation

--- Exercise/Assets/Scripts/Ticking.cs
+++ Exercise/Assets/Scripts/Ticking.cs
@@ -3,28 +3,54 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 using System.Threading.Tasks;
+using UnityEngine.UIElements;
 
 
 
 public class Ticking : MonoBehaviour
 {
     [SerializeField]
-    Transform hoursPivot, minutesPivot, secondsPivot;
+    Transform minutesPivot, secondsPivot, hoursPivot;
 
-    void Awake() { 
-        Debug.Log(DateTime.Now);
-        hoursPivot.localRotation = Quaternion.Euler(0, 0, -30);
-    }
-
-    // Start is called before the first frame update
-    void Start()
+    // Awake can stay void since we won't await an infinite task
+    void Awake()
     {
+        
+        SetInMotion();
         
     }
 
-    // Update is called once per frame
-    async Task Update()
+    async void SetInMotion()
     {
-        //await 
+        minutesPivot.localRotation = Quaternion.Euler(0, 0, DateTime.Now.Minute * (-6));
+
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, DateTime.Now.Hour * (-30) + (DateTime.Now.Minute * (-1)/4));
+
+        await Move(secondsPivot, 1000, DateTime.Now.Second * (-6));
+    }
+
+    async Task Move(Transform myTransform, int delay, int currentPosition) {
+
+        Debug.Log("Current DateTime: " + DateTime.Now);
+        while (true) { 
+           
+           myTransform.localRotation = Quaternion.Euler(0, 0, currentPosition);
+            if (currentPosition % 360 == 0) {
+                minutesPivot.Rotate(0,0,-6);
+                if (currentPosition % 4 == 0) { 
+                   hoursPivot.Rotate(0,0,-1);
+                }
+                
+            }
+            currentPosition -= 6;
+           await Task.Delay(delay); 
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+     
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Unity_game
-A simple game in unity for exercise
+A simple game in unity for exercise, following the tutorials from this webpage https://catlikecoding.com/unity/tutorials/

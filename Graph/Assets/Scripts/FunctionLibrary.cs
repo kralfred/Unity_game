dd09c057428913aa2fb2f7487658d804daadd772
alfred <alfred.withers@seznam.cz>
2025-03-24 04:43:35 +0100

    added spis function and sphere

--- Graph/Assets/Scripts/FunctionLibrary.cs
+++ Graph/Assets/Scripts/FunctionLibrary.cs
@@ -1,38 +1,78 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
 
-    public delegate float Function(float x, float t);
+    public delegate Vector3 Function(float u, float v, float t);
 
 
 
-    public enum FunctionName { Wave, MultiWave, Ripple }
-    static Function[] functions = { Wave, MultiWave, Ripple };
+    public enum FunctionName { Wave, MultiWave, Ripple, Sphere }
+    static Function[] functions = { Wave, MultiWave, Ripple , Sphere };
 
     public static Function GetFunction(FunctionName functionName) {
 
         return functions[(int)functionName];
     }
 
-    public static float Wave(float x, float t)
+    public static Vector3 Wave(float u, float v, float t)
     {
-        return Sin(PI * (x + t));
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + v + t));
+        p.z = v;
+        return p;
     }
-    public static float MultiWave(float x, float t)
+    public static Vector3 MultiWave(float u, float v, float t)
     {
-        float y = Sin(PI * (x + t));
-        y += 0.5f * Sin(2f * PI * (x + t));
-        return y * (2f / 3f);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (u + 0.5f * t));
+        p.y += 0.5f * Sin(2f * PI * (v + t));
+        p.y += Sin(PI * (u + v + 0.25f * t));
+        p.y *= 1f / 2.5f;
+        p.z = v;
+        return p;
     }
-    public static float Ripple(float x, float t)
+
+    public static Vector3 Ripple(float u, float v, float t)
     {
-        float d = Abs(x);
-        float y = Sin(PI * (4f * d - t));
-        return y / (1f + 10f * d);
+        float d = Sqrt(u * u + v * v);
+        Vector3 p;
+        p.x = u;
+        p.y = Sin(PI * (4f * d - t));
+        p.y /= 1f + 10f * d;
+        p.z = v;
+        return p;
     }
+
+    public static Vector3 Sphere(float u, float v, float t)
+    {
+        
+        float r = Cos(0.5f * PI * v);
+        Vector3 p;
+
+
+        p.x = r * Sin(PI * u);
+        p.y = Sin(PI * 0.5f * v);
+        p.z = r * Cos(PI * u);
+
+        return SpinObject(p, t);
+    }
+    public static Vector3 SpinObject(Vector3 vector, float speed) {
+
+        Vector3 returnValue;
+        var cosT = Cos(speed);
+        var sinT = Sin(speed);
+        returnValue.x = cosT * vector.x + sinT * vector.z;
+        returnValue.y = vector.y;
+        returnValue.z = -sinT * vector.x + cosT * vector.z;
+        return returnValue;
+    }
+
 }
--- Graph/Assets/Scripts/Graph.cs
+++ Graph/Assets/Scripts/Graph.cs
@@ -18,12 +18,25 @@ public class Graph : MonoBehaviour
     [SerializeField, Range(1, 100)]
     int resolution = 30;
 
+    [SerializeField] 
+    Button spin;
 
-    [SerializeField]
-    FunctionLibrary.FunctionName function;
+
+    [SerializeField] private FunctionLibrary.FunctionName _function; // Backing field
+    public FunctionLibrary.FunctionName Function
+    {
+        get => _function;
+        set
+        {
+            if (_function != value)
+            {
+                _function = value;
+                UpdateWaveFunction();
+            }
+        }
+    }
 
     Transform[] points;
-
 
     FunctionLibrary.Function waveFunction;
 
@@ -31,49 +44,56 @@ public class Graph : MonoBehaviour
     {
     }
 
-    private void Awake()
+    void Awake()
     {
 
-
-        waveFunction = FunctionLibrary.GetFunction(function);
-
-
-
-        points = new Transform[resolution];
+        UpdateWaveFunction();
 
         float step = 2f / resolution;
-
-        var position = Vector3.zero;
         var scale = Vector3.one * step;
 
-
+        points = new Transform[resolution * resolution];
         for (int i = 0; i < points.Length; i++)
         {
+
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
+
             point.localScale = scale;
+            point.SetParent(transform, false);
         }
-        InitiateAsync();
     }
-    async void InitiateAsync()
+    private void OnValidate()
     {
-        await SwitchWavesAsync();
+        UpdateWaveFunction();
     }
+
+    void UpdateWaveFunction() {
+        waveFunction = FunctionLibrary.GetFunction(_function);
+    }
+
 
     void Update()
     {
-
+        
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-
-            position.y = waveFunction(position.x, time);
-
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition =  waveFunction(u, v, time);
+            
         }
+    }
+
+    async void InitiateAsync()
+    {
+        await SwitchWavesAsync();
     }
 
     async Task SwitchWavesAsync()
@@ -90,7 +110,5 @@ public class Graph : MonoBehaviour
         }
 
     }
-
-
 
 }
